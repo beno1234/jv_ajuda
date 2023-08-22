e3041041929967aca65c81c8a5a74476d5c781d6
beno1234 <benolopesdias@gmail.com>
2023-08-22 14:52:43 -0300

    layout novo novo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HeaderSection = () => {
   return (
     <header className="h-[100vh]">
--- src/pages/About/Page2.tsx
+++ src/pages/About/Page2.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AboutUsSection = () => {
   return (
     <section className="py-16 bg-gray-100">
